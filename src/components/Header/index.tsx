1035c16eb7f83dc21d0b4f8c15214e25cb3b96a7
William <wilz.kelvin12@gmail.com>
2022-01-22 21:25:08 -0300

    Started the Fylo challenge, configured the next app and finished the header.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss';
+import { ImStack } from 'react-icons/im';
+
+export function Header() {
+    return (
+        <header className={styles.container}>
+            <section className={styles.section}>
+                <div>
+                    <ImStack color="hsl(0, 0%, 100%)" size={25}/>
+                    <strong>Fylo</strong>
+                </div>
+                <nav>
+                    <strong>Features</strong>
+                    <strong>Team</strong>
+                    <strong>Sign In</strong>
+                </nav>
+            </section>
+        </header>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Header } from '../components/Header'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
